a2a219aad77b76a60df219e1ede0481665b88d3f
fra1ny <113284520+fra1ny@users.noreply.github.com>
2022-09-11 20:28:20 +0300

    v1.1

--- tamagochi.py
+++ tamagochi.py
@@ -6,9 +6,12 @@ pet_name = str(input("Введите имя вашего питомца: "))
 hunger = 100 # шкала голода
 health = 100 # шкала здоровья
 fatigue = 100 # шкала утомлённости
+age = 0 # дней питомцу
 
 def print_stats(): # печатаем статистику питомца
-	print(".-.-.-.-.-.-.-.-.-Статистика-.-.-.-.-.-.-.-.-.\nУровень голода вашего питомца:", hunger, "\nУровень здоровья вашего питомца:", health, "\nУровень утомлённости вашего питомца:", fatigue, "\n(^˵OωO˵^)\n.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.")
+	global age
+	age = age + 1
+	print(".-.-.-.-.-.-.-.-.-Статистика-.-.-.-.-.-.-.-.-.\nУровень голода вашего питомца:", hunger, "\nУровень здоровья вашего питомца:", health, "\nУровень утомлённости вашего питомца:", fatigue, "\nДней питомцу:", age, "\n(^˵OωO˵^)\n.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.")
 	time.sleep(2)
 
 def skip_time(): # пропускаем 1 день
@@ -42,6 +45,7 @@ def treat(): # лечим питомца
 	global hunger, health, fatigue, pet_name
 	print("Вы вылечили питомца", pet_name)
 	hunger = hunger - 20
+	fatigue = fatigue - 20
 	health = 100
 	time.sleep(2)
 	choise()
@@ -70,18 +74,18 @@ def stats_check(): # проверяем статы питомца
 		health = 0
 
 	if health == 0:
-		print("\nВаш питомец умер\n(^˵XwX˵^)\nGame Over")
+		print("\n.-.-.-.-.-.-.-.-.-Ваш питомец умер-.-.-.-.-.-.-.-.-.\n(^˵XwX˵^)\nGame Over\nПрожито:", age, "дней")
 		input()
 		exit()
 	elif fatigue == 0:
-		print("\nВаш питомец заскучал\n(^˵=w=˵^)\nGame Over")
+		print("\n.-.-.-.-.-.-.-.-.-Ваш питомец заскучал-.-.-.-.-.-.-.-.-.\n(^˵#w#˵^)\nGame Over\nПрожито:", age, "дней")
 		input()
 		exit()
 
 def illness(): # шанс питомца заболеть
 	global hunger, health, fatigue, pet_name
 	illness_chanse = random.randint(0, 100)
-	if illness_chanse <= 5:
+	if illness_chanse <= 10:
 		print(pet_name, "заболел. Похоже, его стоит вылечить")
 		health = health - 20
 	else:
